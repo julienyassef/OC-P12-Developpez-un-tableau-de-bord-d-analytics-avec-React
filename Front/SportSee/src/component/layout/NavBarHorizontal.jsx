e18a7b81595025b2643404ca1f6494e54c285b27
julien yassef <juyassef@gmail.com>
2024-01-24 16:44:30 +0100

    création navbar horizontal et son css

--- Front/SportSee/src/component/layout/NavBarHorizontal.jsx
+++ Front/SportSee/src/component/layout/NavBarHorizontal.jsx
@@ -1,8 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
 import './NavBarHorizontal.css'
+
+import logo from '../../assets/logo.svg'
+import nameSite from '../../assets/nameSite.svg'
 
 function NavBarHorizontal() {
   return (
-    <div>NavBarHorizontal</div>
+    <div className='NavBarHorizontal'>
+        <div className="container-logo">
+            <img  className='logo'src={logo} alt="logo de Sport See" />
+            <img  className='nameSite'src={nameSite} alt="nom du site web : Sport See" />
+        </div>
+        <a>Accueil</a>
+        <a>Profil</a>
+        <a>Réglage</a>
+        <a>Communauté</a>
+    </div>
   )
 }
 export default NavBarHorizontal
