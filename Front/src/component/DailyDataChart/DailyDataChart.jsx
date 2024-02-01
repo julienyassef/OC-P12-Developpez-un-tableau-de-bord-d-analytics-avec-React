9f7629afc5a1c62bb4707309f4ad73e5a9702b6e
julien yassef <juyassef@gmail.com>
2024-02-01 16:50:23 +0100

    tentative de modif donné et css charbar, à revoir

--- Front/src/component/DailyDataChart/DailyDataChart.jsx
+++ Front/src/component/DailyDataChart/DailyDataChart.jsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 //Recharts
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Text, ResponsiveContainer } from 'recharts';
 
 // Data
 import { getUserActivityData } from '../../apiService/apiService';
@@ -33,17 +33,30 @@ function DailyDataChart({userId}) {
         return <p>Loading...</p>;
     }
 
+    //modification de l'affichage des dates sur le graphique
+    const extractDay = (dateString) => {
+      // Divise la chaîne de date en parties en utilisant le caractère '-'
+      const parts = dateString.split('-');
+      // Récupère le dernier élément du tableau résultant
+      const day = parts[2];
+      // Utilise parseInt pour convertir la chaîne en entier
+      return parseInt(day, 10);
+    };
+ 
     
     return (
-      <BarChart width={835} height={320} data={userData.data.sessions}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="day" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="kilogram" fill="#282D30" name="Poids (Kg)" />
-      <Bar dataKey="calories" fill="#E60000" name="Calories brûlées (KCal)" />
-      </BarChart>
-  )
+      <ResponsiveContainer width={835} height={320}>
+        <BarChart width={835} height={320} data={userData.data.sessions}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis className='barChart-XAxis' dataKey={(data) => extractDay(data.day)} />
+          <YAxis orientation="right" />
+          <Tooltip />
+          <Legend verticalAlign="top" />
+          <Text className='barChart-title'verticalAlign="top"> Activité quotidienne </Text>
+          <Bar className='barChart-Bar' dataKey="kilogram" fill="#282D30" name="Poids (Kg)" />
+          <Bar className='barChart-Bar' dataKey="calories" fill="#E60000" name="Calories brûlées (KCal)" />
+        </BarChart>
+      </ResponsiveContainer>
+    );
 }
 export default DailyDataChart
