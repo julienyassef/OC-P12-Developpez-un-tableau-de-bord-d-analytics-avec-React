d2dc0167a7d060083ac47729c4a735a24dba1ba2
julien yassef <juyassef@gmail.com>
2024-02-08 11:35:54 +0100

    modif radarchart nom légend

--- Front/src/component/ActivityCarriedOut/ActivityCarriedOut.jsx
+++ Front/src/component/ActivityCarriedOut/ActivityCarriedOut.jsx
@@ -5,22 +5,43 @@ import useData from '../../hooks/useData';
 //React
 import React from 'react';
 
-import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Legend } from 'recharts';
+import { RadarChart, PolarGrid, PolarAngleAxis, Radar, ResponsiveContainer } from 'recharts';
 
 
 
 function ActivityCarriedOut() {
   const {userPerformanceData} = useData()
- 
 
+// Fonction pour récupérer le type "kind" dans un tableau
+const getKindArray = () => {
+  return Object.values(userPerformanceData.kind);
+}
+ 
   return (
-    <RadarChart outerRadius={90} width={730} height={250} data={userPerformanceData.data}>
-      <PolarGrid />
-      <PolarAngleAxis dataKey="kind" />
-      <PolarRadiusAxis angle={30} domain={[0, 150]} />
-      <Radar name="Performance" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-      <Legend />
-    </RadarChart>
+    <ResponsiveContainer width={258} height={250}>
+      <RadarChart
+      className='RadarChart'
+        cx="50%"
+        cy="50%"
+        outerRadius="70%"
+        data={userPerformanceData.data} 
+      >
+        <PolarGrid radialLines={false} />
+        <PolarAngleAxis
+          dataKey="kind"
+          style={{ color: "#000000", fontSize: "11" }}
+          tick={{ fill: "#ffffff", fontSize: 20 }}
+          tickFormatter={(value) => getKindArray()[value - 1]}
+        />
+
+        <Radar
+          dataKey="value"
+          stroke="red"
+          fill="red"
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    </ResponsiveContainer>
   )
 }
 export default ActivityCarriedOut
