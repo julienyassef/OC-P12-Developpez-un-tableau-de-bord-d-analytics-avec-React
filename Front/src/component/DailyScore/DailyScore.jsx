417c0375b24c216d3949ff8fd2680bd9f5a979c0
julien yassef <juyassef@gmail.com>
2024-02-08 15:02:06 +0100

    daily chart css et graph

--- Front/src/component/DailyScore/DailyScore.jsx
+++ Front/src/component/DailyScore/DailyScore.jsx
@@ -1,52 +1,74 @@
-import './DailyScore.css'
+import './DailyScore.css';
 
-//React
+// React
 import React from 'react';
 
-//Rechart
-import { RadialBarChart, RadialBar, Legend, Tooltip } from 'recharts';
+// Rechart
+import { PieChart, Pie, Cell, Text, Label } from 'recharts';
 
-//Data
+// Data
 import useData from '../../hooks/useData';
 
-
-
 function DailyScore() {
-    const {userData} = useData();
+  const { userData } = useData();
+  const { score, todayScore } = userData;
 
-  
-    const { score, todayScore } = userData;
+  // Choisissez le score à utiliser, en priorisant todayScore s'il est défini
+  const selectedScore = todayScore !== undefined ? todayScore : score;
 
-    // Choisissez le score à utiliser, en priorisant todayScore s'il est défini
-    const selectedScore = todayScore !== undefined ? todayScore : score;
-    
-    // Convertir le score en pourcentage
-    const scorePercentage = Math.round(selectedScore * 100);
+  // Convertir le score en pourcentage
+  const scorePercentage = Math.round(selectedScore * 100);
+
+  // divisé les % en donnée
+  const data = [
+    { name: 'Reste', value: 360 - scorePercentage },
+    { name: 'Score', value: scorePercentage }
+  ];
+
+  // Couleurs pour le graphique % journée score
+  const colors = [ 'none', '#FF0000'];
 
   return (
     <div className="daily-score">
-    <p>{`${scorePercentage}%`}</p>
-    <RadialBarChart
-        width={300}
-        height={300}
-        innerRadius="80%"
-        outerRadius="100%"
-        data={[{ uv: scorePercentage }]}
-        startAngle={0}
-        endAngle={360}
-        >
-        <RadialBar
-            minAngle={0}
-            label={{ fill: '#FF0000', position: 'insideStart' }}
-            fill='gray'
-            clockWise={true}
-            dataKey='uv'
-            maxAngle={scorePercentage} 
-        />
-        <Tooltip />
-        </RadialBarChart>
-
-  </div>
-  )
+      <h3 className='h3-dayly-score'>Score</h3>
+      <div className="container-pourcentage">
+        <p className='text-pourcentage-title' >{`${scorePercentage}%`} 
+          <span className='text-pourcentage-content'> de votre </span>
+          <span className='text-pourcentage-content'> objectif</span>
+        </p>
+      </div>
+      <div className="container-chart ">
+        <PieChart  className= 'toto'
+        width={250} height={250}>
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            cx={120}
+            cy={120}
+            innerRadius={60}
+            outerRadius={80}
+            cornerRadius={10}
+            startAngle={-180}
+            
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+          <Pie
+            data={data}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            innerRadius="0%"
+            outerRadius="50%"  
+            fill="#ffffff"  
+          />
+        </PieChart>
+      </div>
+    </div>
+  );
 }
-export default DailyScore
+
+export default DailyScore;
