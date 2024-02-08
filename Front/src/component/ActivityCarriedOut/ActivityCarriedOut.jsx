c564d3dc38d44cbe0aa038283a9b8b032ae49596
julien yassef <juyassef@gmail.com>
2024-02-08 11:50:38 +0100

    renvoit les donné du radarchgart en français et change les positions

--- Front/src/component/ActivityCarriedOut/ActivityCarriedOut.jsx
+++ Front/src/component/ActivityCarriedOut/ActivityCarriedOut.jsx
@@ -12,10 +12,20 @@ import { RadarChart, PolarGrid, PolarAngleAxis, Radar, ResponsiveContainer } fro
 function ActivityCarriedOut() {
   const {userPerformanceData} = useData()
 
-// Fonction pour récupérer le type "kind" dans un tableau
+// Fonction pour récupérer le type "kind" dans un tableau en français
 const getKindArray = () => {
-  return Object.values(userPerformanceData.kind);
+  const displayToFrench = {
+    "1": "Cardio",
+    "2": "Energie",
+    "3": "Endurance",
+    "4": "Force",
+    "5": "Vitesse",
+    "6": "Intensité"
+  };
+  // retourne kind selon son numéro et en français
+  return Object.keys(userPerformanceData.kind).map(englishKey => displayToFrench[englishKey]);
 }
+
  
   return (
     <ResponsiveContainer width={258} height={250}>
@@ -31,7 +41,7 @@ const getKindArray = () => {
           dataKey="kind"
           style={{ color: "#000000", fontSize: "11" }}
           tick={{ fill: "#ffffff", fontSize: 20 }}
-          tickFormatter={(value) => getKindArray()[value - 1]}
+          tickFormatter={(value, index) => getKindArray()[getKindArray().length - index - 1]} // inverser l'autre et décallé d'un cran 
         />
 
         <Radar
